622780d51fba30c1eaa6c54ec44f96b9f01a585e
saifi369 <chsaif369@gmail.com>
2020-12-02 00:20:29 +0500

    Navigation with NavController

--- app/src/main/java/com/saif/jetpacknavigationexample/HomeFragment.kt
+++ app/src/main/java/com/saif/jetpacknavigationexample/HomeFragment.kt
@@ -1,5 +1,39 @@
 package com.saif.jetpacknavigationexample
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+
+        btn_view_balance.setOnClickListener {
+            val navOption = NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_in_left)
+                .setPopExitAnim(R.anim.slide_out_right)
+                .build()
+
+            navController.navigate(R.id.viewBalanceFragment,null,navOption)
+        }
+
+        btn_transactions.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewTransactionsFragment()
+            navController.navigate(action)
+        }
+
+        btn_send_money.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToChooseReceiverFragment()
+            navController.navigate(action)
+        }
+
+    }
+
+}
